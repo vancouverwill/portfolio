d9d0538829094a1f8053509542afff600fa78811
William Melbourne <will_melbourne@hotmail.com>
2012-04-05 17:38:02 -0700

    added model admin for terms - added files

---
+++ mysite/code/MyTermAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+class MyTermAdmin extends ModelAdmin {
+    
+  public static $managed_models = array(   //since 2.3.2
+      'Term'
+   );
+ 
+  static $url_segment = 'terms'; // will be linked as /admin/products
+  static $menu_title = 'My Term Admin';
+ 
+}
+?>
